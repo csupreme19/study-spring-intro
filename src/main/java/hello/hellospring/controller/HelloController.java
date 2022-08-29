44cae518f4dedbe066cc13111600c5238583c88c
최승훈 (AI Industry플랫폼개발TF) <seunghoon.choi@kt.com>
2022-08-29 16:03:02 +0900

    ResponseBody 컨트롤러

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,7 +2,10 @@ package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +14,36 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @RequestMapping("hello-mvc")
+    public String hello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @RequestMapping("hello-string")
+    public @ResponseBody String helloString(@RequestParam("name") String name) {
+        return name;
+    }
+
+    @GetMapping("hello-api")
+    public @ResponseBody Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        // 자바 빈 규약
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
