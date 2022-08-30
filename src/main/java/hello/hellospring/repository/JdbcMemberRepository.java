5b1bcd044c904f448d027cdb8cef4b8b1c96c922
최승훈 (AI Industry플랫폼개발TF) <seunghoon.choi@kt.com>
2022-08-30 19:16:14 +0900

    Spring 통합 테스트

--- src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -137,6 +137,10 @@ public class JdbcMemberRepository implements MemberRepository {
         return DataSourceUtils.getConnection(dataSource);
     }
 
+    private void closeConnection(Connection conn) {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
     private void close(Connection conn, Statement stmt, ResultSet rs) {
         try {
             if(rs != null) rs.close();
@@ -148,28 +152,6 @@ public class JdbcMemberRepository implements MemberRepository {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        try {
-            if(conn != null) conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
-        try {
-            if(rs != null) rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            if (pstmt != null) pstmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            if(conn != null) conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        if(conn != null) closeConnection(conn);
     }
 }
